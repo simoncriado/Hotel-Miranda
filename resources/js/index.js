018b23a2cf071d20632ca2951d15c65da1d97136
Simon Criado <50947741+simoncriado@users.noreply.github.com>
2022-11-30 17:20:26 +0100

    Working on homepage: implement swipers

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,50 @@
-let swiperRoomRelated = new Swiper(".swiper", {
-  effect: "default",
+// Setting the amount of slides visible in the screen based on the window´s width
+const responsiveSwiper = (screenWidth) => {
+  let numSlides = 0;
+  if (screenWidth > 1000) {
+    numSlides = 3;
+  } else {
+    numSlides = 1;
+  }
+  return new Swiper(".swiper", {
+    effect: "default",
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: numSlides,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 6000,
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+};
+
+// Creating a swiper based with the window´s width as a parameter
+let swiper = responsiveSwiper(window.innerWidth);
+
+window.addEventListener("resize", () => {
+  swiper = responsiveSwiper(innerWidth);
+});
+
+const swiperFeatures = new Swiper(".swiperFeatures", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: "1",
+  spaceBetween: 30,
+  autoplay: {
+    delay: 6000,
+  },
+  pagination: {
+    el: ".swiperFeatures-pagination",
+    type: "bullets",
+  },
+});
+
+const swiperMenu = new Swiper(".swiperMenu", {
   direction: "horizontal",
   loop: true,
   slidesPerView: "1",
@@ -7,14 +52,8 @@ let swiperRoomRelated = new Swiper(".swiper", {
   autoplay: {
     delay: 4000,
   },
-  // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
+    nextEl: ".swiperMenu-button-next",
+    prevEl: ".swiperMenu-button-prev",
   },
 });
